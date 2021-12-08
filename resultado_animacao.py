7400339edc05e075a7c64a318738fd16d238934d
lucascm@tecgraf.puc-rio.br <lucascm@tecgraf.puc-rio.br>
2021-12-08 16:14:23 -0300

    Removed ffmpeg usage

--- resultado_animacao.py
+++ resultado_animacao.py
@@ -5,10 +5,8 @@ import numpy as np
 import random
 import flood
 
-plt.rcParams['animation.ffmpeg_path'] = 'D:/ffmpeg/bin/ffmpeg.exe'
-
-gridsize = 1000
-cmap = colors.ListedColormap(['#FFFFFF','#ffc5d0', '#ffff00', '#22cae0', '#f52ec0', '#603f8b', '#b4fee7', '#a16ae8', '#fd49a0', '#821d30'])
+gridsize = 100
+cmap = colors.ListedColormap(['#FFFFFF','#ffc5d0', '#ffff00', '#22cae0', '#f52ec0', '#603f8b', '#b4fee7', '#a16ae8', '#fd49a0', '#821d30', '#d3223d'])
 bounds = [0,1,2,3,4,5,6, 7, 8]
 norm = colors.BoundaryNorm(bounds, cmap.N)
 fig, ax = plt.subplots(figsize = (10, 10))
@@ -23,31 +21,31 @@ ax.set_yticklabels([])
 ax.set_xticklabels([])
 
 flood_obj = flood.Flood(gridsize)
-flood_obj.set_seeds(8)
+flood_obj.set_seeds(7)
 
 data_history = [[[]]]
 data_history.append(flood_obj.data.copy())
 
 control = gridsize//2
 while control > 1:
+    print(f"Step: {control}")
     flood_obj.jump_flood_iteration()
     data_history.append(flood_obj.data.copy())
     control = control //2
 
 flood_obj.step = control = 32
 
-
+control = gridsize//2
 while control > 1:
+    print(f"Step: {control}")
     flood_obj.jump_flood_iteration()
     data_history.append(flood_obj.data.copy())
     control = control //2
 
 def draw_frame(frame_number):
-    
     updated_image.set_array(data_history[frame_number])
     return [updated_image]
 
-
-anim = animation.FuncAnimation(fig, draw_frame, frames=(len(data_history)), interval=2000, blit=True)
-FFwriter = animation.FFMpegWriter()
-anim.save("resultd.mp4", writer=FFwriter)
+print(len(data_history))
+anim = animation.FuncAnimation(fig, draw_frame, frames=(len(data_history)), interval=1000, blit=False)
+anim.save("resultd.gif")
--- resultado_imagem.py
+++ resultado_imagem.py
@@ -4,9 +4,9 @@ import numpy as np
 import flood
 
 
-gridsize = 100
+gridsize = 1000
 flood_obj = flood.Flood(gridsize)
-flood_obj.set_seeds(10)
+flood_obj.set_seeds(40)
 start_data = flood_obj.data.copy()
 seeds_x = []
 seeds_y = []
@@ -18,6 +18,7 @@ for value in flood_obj.colors_dict.values():
 control = gridsize//2
 
 while control > 1:
+    print(f"Step: {control}")
     flood_obj.jump_flood_iteration()
     control = control //2
 
@@ -25,26 +26,14 @@ flood_obj.step = 8
 control = 8
 
 while control > 1:
+    print(f"Step: {control}")
     flood_obj.jump_flood_iteration()
     control = control //2
 
 
-
-
 data = flood_obj.data
-figure, axes = plt.subplots(1, 2)
-axes[0].imshow(data)
-axes[0].scatter(seeds_x, seeds_y, color="red")
 
-
-
-flood_obj.data = start_data
-for i in range(gridsize):
-    flood_obj.common_flood_iteration()
-
-data2 = flood_obj.data
-axes[1].imshow(data2)
-axes[1].scatter(seeds_x, seeds_y, color="red")
-
+plt.imshow(data)
+plt.scatter(seeds_x, seeds_y, color="red", marker='.')
 
 plt.show()
